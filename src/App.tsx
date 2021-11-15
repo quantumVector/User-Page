5c3fdf557317faa8f11bfbd722b86bb36d65af60
quantumVector <payned@mail.ru>
2021-11-16 02:55:59 +0300

    added basic project architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import { Login, NotFound, SignUp, UserPage } from './pages';
 
 function App() {
   return (
-    <div className="app-wrapper">
+    <div className='app-wrapper'>
+      <Routes>
+        <Route path='/' element={<UserPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Login } from './Login';
+export { default as SignUp } from './SignUp';
+export { default as UserPage } from './UserPage';
+export { default as NotFound } from './NotFound';

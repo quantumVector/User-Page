c0499f423e92e5e9f18f8f53536e80c3112fc851
quantumVector <payned@mail.ru>
2021-11-16 10:34:26 +0300

    added login layout

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,10 +1,43 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
+import { Alert, Button, Grid, TextField, Box } from '@mui/material';
 
 const Login: FC = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      {false && (
+        <Alert severity='error' style={{ marginTop: 20 }}>
+          {'error'}
+        </Alert>
+      )}
+      <Grid display='flex' justifyContent='center' alignItems='center' height=''>
+        <form>
+          <TextField
+            type='email'
+            label='Email'
+            variant='outlined'
+            fullWidth
+            sx={{ display: 'block', marginTop: 3, marginBottom: 3, width: 400 }}
+            required
+          />
+          <TextField
+            type='password'
+            label='Password'
+            variant='outlined'
+            fullWidth
+            sx={{ display: 'block', marginBottom: 3, width: 400 }}
+            required
+          />
+          <Box textAlign='center'>
+            <Button
+              variant='outlined'
+              size="large"
+            >
+              Login
+            </Button>
+          </Box>
+        </form>
+      </Grid>
+    </>
   )
 }
 

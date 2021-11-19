8cf5a0b871125f1fd39c9ff7ff3f1fbdcde813b9
quantumVector <payned@mail.ru>
2021-11-19 18:15:26 +0300

    added env file

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import * as firebase from 'firebase/app';
 import AppRoutes from './components/routes/Routes';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './components/providers/AuthProvider';
-
-firebase.initializeApp({
-  apiKey: "AIzaSyDCpry19k3lALtiix4kQ3I4zzijM3LW14o",
-  authDomain: "user-page-3cfeb.firebaseapp.com",
-  projectId: "user-page-3cfeb",
-  storageBucket: "user-page-3cfeb.appspot.com",
-  messagingSenderId: "74694622364",
-  appId: "1:74694622364:web:6651db478f9b970af14159",
-  measurementId: "G-7WN52ZJP4C"
-});
+import './firebase';
 
 ReactDOM.render(
   <React.StrictMode>
